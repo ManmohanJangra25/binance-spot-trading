30b436f4a2d3017df84d3b0540dc547d8238f1d5
Manmohan <manmohanjangra2528@gmail.com>
2024-12-12 15:13:07 +0530

    Updates

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,13 @@
+import "./header.css";
+
 export default function Header() {
   return (
-    <>
-      Hello
-    </>
+    <div className="container-fluid main_header d-flex align-center">
+      <header>
+        <div className="headerLogo d-flex align-center">
+          <img src="./assets/images/binance_logo.png" alt="logo" width="120px"/>
+        </div>
+      </header>
+    </div>
   )
 }
